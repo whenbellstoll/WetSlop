431f21b617c6e194aaeb6bbcc4c24ece0cd91741
Kent Reese (RIT Student) <kxr6031@rit.edu>
2025-01-26 16:23:19 -0500

    Final SFX

--- Controls.gd
+++ Controls.gd
@@ -6,6 +6,8 @@ var curPos : Vector2
 var swiping = false
 var Hand : Node3D
 var threshold = 10
+@onready var sfx = $"../AudioStreamPlayer3D"
+var played = false
 
 func _ready():
 	Hand = get_tree().get_root().get_node("Node3D/Hand")
@@ -17,5 +19,13 @@ func _process(delta):
 		Hand.transform.origin = Vector3(Hand.transform.origin.x, Hand.transform.origin.y + delta, Hand.transform.origin.z)
 	if Input.is_action_pressed("wheel_down") && Hand.transform.origin.y > 0.6:
 		Hand.transform.origin = Vector3(Hand.transform.origin.x, Hand.transform.origin.y - delta, Hand.transform.origin.z)
-			
+	
+	if Hand.transform.origin.y < 0.8 && !played && Hand.transform.origin.y > 0.7:
+		sfx.play()
+		played = true
+	elif Hand.transform.origin.y > 0.9 && played :
+		played = false
+		
+		
+	
 	pass
